993ae71696e275252edd8fe3159a36e155693176
jsshin <tb25271@gmail.com>
2024-07-26 17:58:06 +0900

    [feat] store model 과 fetch 쪽 업데이트

--- frontend/stores/fetch/post/post.ts
+++ frontend/stores/fetch/post/post.ts
@@ -1,6 +1,17 @@
-import { ErrorType } from "../customAxios";
+import { customAxios, ErrorType } from "../customAxios";
+import { UseMutationOptions } from "@tanstack/react-query";
+import type { PostModel } from "../../model/post/post.model";
+type AwaitedInput<T> = PromiseLike<T> | T;
 
-export const getPostList = <
-  TError = ErrorType<unknown>,
-  TContext = unknown
->(options?: {}) => {};
+type Awaited<O> = O extends AwaitedInput<infer T> ? T : never;
+
+export const postPosts = () => {
+  return customAxios<void>({ url: `/v1/post`, method: "POST" });
+};
+
+export const getPosts = <TError = ErrorType<unknown>, TContext = unknown>(options?: {
+  mutation: UseMutationOptions<Awaited<ReturnType<typeof PostMo>>,
+  TError,
+  void,
+  TContext,
+}) => {};
---
+++ frontend/stores/model/post/post.model.ts
@@ -0,0 +1,6 @@
+export type PostModel = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+};
